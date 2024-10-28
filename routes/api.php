8dd50abf399f52a9a1414bfcc70a889b06d543ee
Binura99 <binurayasith12@gmail.com>
2024-10-28 22:24:00 +0530

    Add exam duty

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExamDutyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LecturerController;
@@ -13,6 +14,12 @@ Route::prefix('v1')->group(function () {
     Route::post('lecturers', [LecturerController::class, 'store']);
     Route::put('lecturers/{id}', [LecturerController::class, 'update']);
     Route::delete('lecturers/{id}', [LecturerController::class, 'destroy']);
+
+    Route::get('exam-duties/getAll', [ExamDutyController::class, 'index']);
+    Route::get('exam-duties/{id}', [ExamDutyController::class, 'show']);
+    Route::post('exam-duties/add', [ExamDutyController::class, 'store']);
+    Route::put('exam-duties/edit/{id}', [ExamDutyController::class, 'update']);
+    Route::delete('exam-duties/{id}', [ExamDutyController::class, 'destroy']);
 });
 
 // api url: api/v1/lecturers
